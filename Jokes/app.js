e771d85c087e405581974f16435016f5bcf6d267
HELENA POLO TORCAL <helenapolotorcal@gmail.com>
2021-06-06 23:43:31 +0200

    ex acudits 2.0

--- Jokes/app.js
+++ Jokes/app.js
@@ -1,36 +1,30 @@
 $(document).ready(function(){
-  $(function(){
-    $.getJSON('https://my-bao-server.herokuapp.com/api/breadpuns', function(data){
-      $("#joke").html(data + "<br>");
-    });
-  });
   animateToast('#toast1');
   animateToast('#toast2');
   animateToast('#toast3');
   animateToast('#toast4');
   animateToast('#toast5');
   animateToast('#toast6');
+  getJoke();
 });
-
-$(function(){
-  $("#button").click(function(e){
-    $.getJSON('https://my-bao-server.herokuapp.com/api/breadpuns', function(data){
-      $("#joke").html(data + "<br>");
-    });
+function getJoke(){
+  $.getJSON('https://my-bao-server.herokuapp.com/api/breadpuns', function(data){
+    $("#joke").html(data + "<br>");
   });
-});
+}
+$("#button").click(getJoke);
 
 function makeNewPosition(){    
-  var height = $(window).height();
-  var width = $(window).width();
-  var newHeight = Math.floor(Math.random() * height);
-  var newWidth = Math.floor(Math.random() * width);
+  let height = $(window).height();
+  let width = $(window).width();
+  let newHeight = Math.floor(Math.random() * height);
+  let newWidth = Math.floor(Math.random() * width);
 
   return [newHeight,newWidth];    
-}
+};
 
 function animateToast(toastId){
-  var newPosition = makeNewPosition();
+  let newPosition = makeNewPosition();
   $(toastId).animate({ top: newPosition[0], left: newPosition[1] }, 15000,   function(){
     animateToast(toastId);        
   });
